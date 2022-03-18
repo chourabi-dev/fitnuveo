ac8116b492ad5e180a4f47a22a898e0ecc62b24a
chourabi taher <tchourabi@gmail.com>
2022-03-18 11:47:37 +0100

    latest

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+
+const routes: Routes = [
+  { path:'', component:SearchPageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
